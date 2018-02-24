4c0d3256308eacabe6fb6c661edc35250719e3ca
Alex Ballera <alexballera@gmail.com>
2018-02-24 16:06:21 -0400

    setattributes main

--- src/components/layout/createMain.js
+++ src/components/layout/createMain.js
@@ -1,9 +1,12 @@
 'use strict'
 import { app } from './app'
+import {setAttributes} from '../../scripts/helpers/setAttributes.js'
 
 export default () => {
   const main = document.createElement('main')
-  main.setAttribute('id', 'main')
-  main.setAttribute('class', 'main')
+  setAttributes(main, {
+    'class': 'main',
+    'id': 'main'
+  })
   app.appendChild(main)
 }
