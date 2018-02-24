99de04eefcdf7a14f556fe09fba0b91e9755c439
Alex Ballera <alexballera@gmail.com>
2018-02-24 16:03:52 -0400

    setattributes footer

--- src/components/layout/createFooter.js
+++ src/components/layout/createFooter.js
@@ -1,9 +1,12 @@
 'use strict'
 import { app } from './app'
+import {setAttributes} from '../../scripts/helpers/setAttributes.js'
 
 const footer = document.createElement('footer')
-footer.setAttribute('id', 'footer')
-footer.setAttribute('class', 'footer')
+setAttributes(footer, {
+  'class': 'footer',
+  'id': 'footer'
+})
 
 export default () => {
   app.appendChild(footer)
