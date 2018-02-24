57278ae81682dee7b8fd5d4b1a9a68d8e02654ec
Alex Ballera <alexballera@gmail.com>
2018-02-24 16:02:17 -0400

    setattributes navbar

--- src/components/layout/createNavbar.js
+++ src/components/layout/createNavbar.js
@@ -1,8 +1,11 @@
 'use strict'
 import { app } from './app'
+import {setAttributes} from '../../scripts/helpers/setAttributes.js'
 
 const navbar = document.createElement('header')
-navbar.setAttribute('id', 'navbar-top')
+setAttributes(navbar, {
+  'id': 'navbar-top'
+})
 
 export default () => {
   app.appendChild(navbar)
