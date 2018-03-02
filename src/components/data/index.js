bef40446fdf195f29228fd570efc690b5e1ff916
Alex Ballera <alexballera@gmail.com>
2018-03-02 09:13:25 -0400

    Agregada funcionalidad path

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -1,5 +1,15 @@
 'use strict'
 
-const path = '/'
-
+var path
+const url = () => {
+  var host = window.location.host
+  if (host === 'localhost:8000') {
+    path = '/'
+    return path
+  } else {
+    path = '/alacancha/'
+    return path
+  }
+}
+url()
 export {path}
