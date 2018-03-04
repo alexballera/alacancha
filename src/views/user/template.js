e4f54a0dd3f8fffddfe904b6ac9a5da4d9feee7b
Alex Ballera <alexballera@gmail.com>
2018-03-04 13:50:27 -0400

    Agregada imagenes bandera

--- src/views/user/template.js
+++ src/views/user/template.js
@@ -3,9 +3,21 @@ import yo from 'yo-yo'
 import {NavItems} from '../../components/navigation'
 
 const elem = yo`
-<section id="seccion-${NavItems.id.id2}" className="container grey lighten-5">
+<section id="seccion-${NavItems.id.id2}" className="container">
   <div id="${NavItems.id.id2}" className="${NavItems.id.id2}">
-    <h1>Estoy en Usuario</h1>
+    <div className="row">
+      <div className="col s12 m9 ${NavItems.id.id2}-contenido">
+        <header className="row">
+          <picture className="col s12 m3">
+            <img src="images/logo-nestle-negro.png" alt=""/>
+          </picture>
+          <div className="col s12 m9">
+            <p>fabulosos premios puedes ganar acertando con tus pronósticos los partidos del mundial...</p>
+          </div>
+        </header>
+      </div>
+      <div className="col s12 m3 ${NavItems.id.id2}-aside">Texto 2</div>
+    </div>
   </div>
   <picture className="${NavItems.id.id2}-bg-logo">
     <img src="images/bg-usuario-logo-bottom.png" alt=""/>
--- src/views/view1/template.js
+++ src/views/view1/template.js
@@ -3,7 +3,7 @@ import yo from 'yo-yo'
 import {NavItems} from '../../components/navigation'
 
 const elem = yo`
-<section id="seccion-${NavItems.id.id1}" className="container grey lighten-5">
+<section id="seccion-${NavItems.id.id1}" className="container">
   <div id="${NavItems.id.id1}" className="${NavItems.id.id1}">
     <h1>Estoy en Vista 1</h1>
   </div>
